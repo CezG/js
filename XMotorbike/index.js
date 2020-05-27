cff566999a4672b4fdb534227e724e2c97539c57
CezG <cezarygulba@gmail.com>
2020-05-27 10:18:41 +0200

    fixed error with drawing random ground

--- XMotorbike/index.js
+++ XMotorbike/index.js
@@ -1,35 +1,36 @@
-let canvas = document.createElement("canvas");
-let context = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 350;
 
+let c = document.createElement("canvas");
+let ctx = c.getContext("2d");
+c.width = 500;
+c.height = 350;
 document.body.appendChild(c);
 
 let perm = [];
 while (perm.length < 255){
-    while(perm.includes(val = Math.floor(Math.random()*255)))
+    while(perm.includes(val = Math.floor(Math.random()*255)));    // drawing random ground
     perm.push(val); 
 }
 
 let lerp = (a,b,t) => a+ (b-a)*t;
+
 let noise = x =>{
     return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 }
 
 
 function loop(){
-    context.fillStyle = "#19f";     
-    context.fillRect(0, 0, canvas.width, canvas.height); // rysowanie 
+    ctx.fillStyle = "#19f";     
+    ctx.fillRect(0, 0, c.width, c.height); // drawing frame
 
-    context.fillStyle = "black";
-    context.beginPath();
-    for (let i = 0; i <canvas.width; i++){
-        context.lineTo(i,noise(i));
-
-
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    for (let i = 0; i <  c.width; i++){
+        ctx.lineTo(i, noise(i));
     }
-    context.fill();
+
+    
+    ctx.fill();
     requestAnimationFrame(loop);
-}
+} 
 
 loop();
