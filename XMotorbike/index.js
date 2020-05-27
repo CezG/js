d7f471720da903e1200782fcd922262fbc3f4155
CezG <cezarygulba@gmail.com>
2020-05-27 18:26:27 +0200

    added rottation

--- XMotorbike/index.js
+++ XMotorbike/index.js
@@ -23,22 +23,34 @@ let player = new function(){
     this.y = 0;
     this.ySpeed = 0;        //gravity
     this.rot = 0;
+    this.rSpeed = 0;
 
     this.img = new Image();
     this.img.src = "motorbike_racing.png";
     this.draw = function(){
         let p1 = c.height - noise(t + this.x) * 0.25;
+        let p2 = c.height - noise(t + 5 + this.x) * 0.25;
+        
+        let grounded = 0;
         if(p1 -15 > this.y){
-            this.ySpeed -= 0.1;         //gravity
+            this.ySpeed += 0.1;         //gravity
         }else{
-            this.y = p1 - 15;          // moving on the ground- stop gravity
-            this.ySpeed = 0;
+            this.y = p1 - 15;           // moving on the ground- stop gravity
+            //this.ySpeed = 0;          // free fall
+            this.ySpeed -= this.y - (p1-15); // reflection effect,  wheel traction
+            grounded = 1;
         }
-
-        this.y -= this.ySpeed;          //falling
-
+        let angle = Math.atan2((p2-15) - this.y, (this.x +5) - this.x);  //atan2 -> 2-argument arctangent in radians
+        this.y += this.ySpeed;          //falling
+        
+        if(grounded){
+            this.rot -= (this.rot - angle) * 0.5;
+            this.rSpeed = this. rSpeed - (angle - this.rot);
+        }
+        this.rot -= this.rSpeed * 0.1;
         ctx.save();
         ctx.translate(this.x, this.y);
+        ctx.rotate(this.rot);
         ctx.drawImage(this.img, -15, -15, 30 , 30);
         ctx.restore();
     }
@@ -46,7 +58,7 @@ let player = new function(){
 
 let t = 0;
 function loop(){
-    t += 1;
+    t += 5;             //time
     ctx.fillStyle = "#19f";     
     ctx.fillRect(0, 0, c.width, c.height); // drawing frame
 
