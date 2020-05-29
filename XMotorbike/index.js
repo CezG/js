506b0ac654f43caed882053494d6dfe2a14c0d64
CezG <cezarygulba@gmail.com>
2020-05-29 11:56:59 +0200

    added sound of motor

--- XMotorbike/index.js
+++ XMotorbike/index.js
@@ -2,13 +2,14 @@
 //need to add turbo  //maybe not
 // turbo for full rotation above the ground //maybe not
 // add flag of start
-// add flag of finish 24km
+// add flag of finish 25km
 // need to see meters to finish or time
 //add best time score
 //add start on the ground
 // go as far a you can and rotate as many you can
 // add flag where last time finished
-
+let soundEngine = new Audio("sounds/harley.mp3");
+        
 
 let c = document.createElement("canvas");
 let ctx = c.getContext("2d");
@@ -17,12 +18,18 @@ c.height = window.innerHeight-20;
 document.body.appendChild(c);
 
 let perm = [];
-while (perm.length < 255){
-    while(perm.includes(val = 2.2*Math.floor(Math.random()*255)));    // drawing random ground, can manipulate hight of the ground
-    perm.push(val); 
+
+function drawingRandomGround(perm){
+    while (perm.length < 255){
+        while(perm.includes(val = 2.2*Math.floor(Math.random()*255)));    // drawing random ground, can manipulate hight of the ground
+        perm.push(val); 
+    }
 }
+drawingRandomGround(perm);
+
 
 let lerp = (a,b,t) => a+(b-a) * (1-Math.cos(t*Math.PI))/2;      //antialiasing
+
 
 let noise = x =>{                                
     x = x * 0.01 % 255;
@@ -38,7 +45,7 @@ let player = new function(){
     this.score = 0;
 
     this.img = new Image();
-    this.img.src = "motorbike_racing.png";
+    this.img.src = "img/motorbike_racing.png";
     this.draw = function(){
         let p1 = c.height - noise(t + this.x) * 0.25;
         let p2 = c.height - noise(t + 5 + this.x) * 0.25;
@@ -91,6 +98,10 @@ let playing = true;
 let k = {ArrowUp:0, ArrowDown:0, ArrowLeft:0, ArrowRight:0};
 function loop(){
     speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.01;
+    
+    if(speed>0)soundEngine.play();
+    if(speed<=0)soundEngine.pause();
+    
     t += 7 * speed;             //position   
     ctx.fillStyle = "#19f";     
     ctx.fillRect(0, 0, c.width, c.height); // drawing frame
